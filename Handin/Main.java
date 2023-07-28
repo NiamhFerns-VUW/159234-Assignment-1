a4b2a4e72453b84fa2c22ee137f4e2d43b17cdf5
Niamh <Niamh.Ferns@proton.me>
2023-07-28 13:01:14 +1200

    Moved to individual repo. Previous commits lost.

---
+++ Handin/Main.java
@@ -0,0 +1,126 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    private static ArrayList<Major> majors;
+    private static ArrayList<Course> courses;
+    private static ArrayList<Lecturer> lecturers;
+    private static String[] locations = {"Auckland", "PN", "Distance"};
+    private static String schoolName = "School of Mathematical and Computational Sciences";
+
+    public static String[] getLocations() { return locations; }
+    private static void printFullName() { System.out.println("School Full Name: " + schoolName); }
+
+    private static void displayInfo() {
+        System.out.println("**************************************");
+        System.out.println("Assignment 1, 159.234 Semester 1 2022");
+        System.out.println("Submitted by: Niamh, Ferns 21007069");
+        System.out.println("My Major at Massey: Computer Science");
+        System.out.println("Assignment 1, 159.234 Semester 1 2022");
+        System.out.println("**************************************");
+        System.out.println();
+    }
+
+    // Read in the values of our semicolon separated strings into the relevant ArrayLists.
+    // This isn't the best way to do this and relies on school_data.txt being perfectly formatted.
+    // It will do fine for this for now.
+    private static void readInData() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("school_data.txt"));
+
+            String expression = "";
+            majors = new ArrayList<Major>();
+            lecturers = new ArrayList<Lecturer>();
+            courses = new ArrayList<Course>();
+
+            while((expression = br.readLine()) != null) {
+                switch (expression.charAt(0)) {
+                    case 'M':
+                        majors.add(new Major(expression.substring(2)));
+                        break;
+                    case 'L':
+                        lecturers.add(new Lecturer(expression.substring(2)));
+                        break;
+                    case 'C':
+                        courses.add(new Course(expression.substring(2), lecturers, majors));
+                        break;
+                }
+            }
+        }
+        catch(IOException e) {
+            System.out.println("Please make sure the \"school_data.txt\" file is present in your program directory.");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        readInData();
+        displayInfo();
+
+        System.out.println("--------------------- Task 1 ---------------------");
+        Main.printFullName();
+        System.out.println();
+
+        System.out.println("--------------------- Task 2 ---------------------");
+        System.out.println("All paper details:");
+        Course.printAllDetails(courses);
+        System.out.println();
+
+        System.out.println("--------------------- Task 3 ---------------------");
+        try {
+            Course[] temp = Major.getByID("CS", majors).getCourses(courses);
+
+            System.out.println("Papers that belong to the Major CS:");
+            Course.simplePrint(temp);
+            System.out.println("Total matching papers in the specified Major - CS: " + temp.length);
+        } catch (NullPointerException e) {
+            System.out.println("No courses found. Error: " + e);
+        }
+        System.out.println();
+
+        System.out.println("--------------------- Task 4 ---------------------");
+        System.out.println("Papers that have an exam:");
+        int i = 0;
+        for (Course c : courses) {
+            if (c.getExamWeighting() > 0.0) { c.simplePrint(); ++i; }
+        }
+        System.out.println("Total number of papers with an exam: " + i);
+        System.out.println();
+
+        System.out.println("--------------------- Task 5 ---------------------");
+        System.out.println("All papers whose assignments weigh more than 50% in total:");
+        i = 0;
+        for (Course c : courses) {
+            if (c.getAssignmentWeighting() > 50.0) { c.simplePrint(); ++i; }
+        }
+        System.out.println("Total number of papers that have assignment weighted more than 50%: " + i);
+        System.out.println();
+
+        System.out.println("--------------------- Task 6 ---------------------");
+        System.out.println("Paper offering details:");
+        for (Course c : courses) {
+            c.printOfferings();
+        }
+        System.out.println();
+
+        System.out.println("--------------------- Task 7 ---------------------");
+        System.out.println("Paper offering details:");
+        Lecturer x = Lecturer.getByID(21007069, lecturers);
+        try {
+            x.printOfferings(courses);
+            System.out.println("I am teaching " + x.getTeaching() + " paper(s)");
+        } catch (NullPointerException e) {
+            System.out.println("This lecturer is not teaching any courses currently");
+        }
+        System.out.println();
+
+        System.out.println("--------------------- Task 8 ---------------------");
+        System.out.println("Paper offering details:");
+        x = Course.getByCode(159234, courses).getLecturer(0);
+        System.out.println("Lecturer's Name: " + x.toString());
+        System.out.println("This lecturer is teaching " + x.getTeaching() + " paper(s).");
+        System.out.println();
+    }
+}
---
+++ src/com/company/Course.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Course {
+    private int code;
+    private String name;
+    private ArrayList<Major> majors;
+    private Double[] assessments = new Double[3];
+    private Modes modes;
+
+    // Constructors for Course
+    public Course(String data, ArrayList<Lecturer> staff, ArrayList<Major> majors) {
+        String[] s = data.split(";");
+        code = Integer.parseInt(s[0]);
+        name = s[1];
+
+        this.majors = new ArrayList<>();
+        String[] m = s[2].split(",");
+        for (String item : m) {
+            this.majors.add(Major.getByID(item, majors));
+        }
+
+        for (int i = 0; i < assessments.length; ++i) {
+            assessments[i] = Double.parseDouble(s[i + 3]);
+        }
+
+        modes = new Modes(Lecturer.assignLecturers(staff.toArray(new Lecturer[0])));
+    }
+
+    // Getters
+    public Double getAssignmentWeighting() { return assessments[0]; }
+    public Double getTestWeighting() { return assessments[1]; }
+    public Double getExamWeighting() { return assessments[2]; }
+    public String getMajors() { return majors.toString(); }
+    public String getName() { return name; }
+    public int getCode() { return code; }
+
+    public static Course getByCode(int code, ArrayList<Course> courses) {
+        for (Course c : courses) {
+            if (c.getCode() == code) { return c; }
+        }
+        return null;
+    }
+
+    // Return the specific lecturer for a specific mode for this course.
+    public Lecturer getLecturer(int n) {
+        return modes.getLecturers()[n];
+    }
+    // Return the specific lecturers for the modes of this course.
+    public Lecturer[] getLecturers() {
+        return modes.getLecturers();
+    }
+
+    // Printers
+    // Print code and name for this course.
+    public void simplePrint() {
+        System.out.println(getCode() + " - " + getName());
+    }
+
+    // Print all the offerings for this course alongside the relevant lecturer.
+    public void printOfferings() {
+        for (int i = 0; i < 3; ++i) {
+            System.out.println(code + " " + modes.getLecturer(i));
+        }
+    }
+
+    // Print all details for every course in an ArrayList
+    public static void printAllDetails(ArrayList<Course> courses) {
+        for (Course c : courses) {
+            System.out.println(c.toString());
+        }
+    }
+
+    // Print code and name for every course in an ArrayList
+    public static void simplePrint(Course[] courses) {
+        for (Course c : courses) {
+            System.out.println(c.getCode() + " - " + c.getName());
+        }
+    }
+
+    // Overrides
+    @Override
+    public String toString() {
+        // Having a string in a loop is super inefficient but for this, the performance loss is tiny.
+        String s =  code + " - " + name + " (";
+        for (Major m : majors) { s += m.getCode() + ", "; }
+        s = s.substring(0, s.length() - 2) + ")";
+        return s;
+    }
+}
